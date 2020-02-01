8b1270102461afb9b10504f22d356d7a2d88eafe
随意 <cosmictinydust@hotmail.com>
2020-02-02 01:17:20 +0800

    设置了Api的路由约定及数据类型的约定

---
+++ Routine.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/companies")]
+    //[Route("api/[controller]")] 也可以用这种方法代替上面的那一句
+    public class CompaniesController:ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompaniesController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository??
+                throw new ArgumentNullException(nameof(companyRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies() 
+        {
+            var companies = await _companyRepository.GetCompaniesAsync();
+            return Ok(companies);
+        }
+
+        [HttpGet("{companyId}")]
+        //[HttpGet]
+        //[Route("{companyId}")]  //可以用注悉掉的这两句代替上面的
+        public async Task<IActionResult> GetCompany(Guid companyId)
+        {
+            var company = await _companyRepository.GetCompanyAsync(companyId);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+    }
+}
--- Routine.Api/Startup.cs
+++ Routine.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Routine.Api.Data;
 using Routine.Api.Services;
 
@@ -26,7 +21,14 @@ namespace Routine.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setup => {
+                setup.ReturnHttpNotAcceptable = true;   //默认值为 false 表示如果此Api的消费者在发送请求是，在请求头的"Accept" 设置为本Api不支持的媒体类型时，不会返回406状态码，而是返回他默认支持的数据类型（默认的数据类型为Json） , 如果设置为 true 就只会返回406
+
+                setup.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());  //为Api的输出数据类型添加xml类型  ，因为是Add操作，返回数据类型默认值还是Json
+                //setup.OutputFormatters.Insert(0, new XmlDataContractSerializerOutputFormatter()); //如果要把添加的xml类型设置为默认的，就用Insert方法，设置Index值为0
+
+            });
+            //.AddXmlDataContractSerializerFormatters();  //.net 3.0后可以使用这种方法
             services.AddScoped<ICompanyRepository, CompanyRepository>();
             //AddScoped这种是针对每一次Http请求都重新生成服务的情况
 
