556354fd6d9846a9b18ca524cafd65206bca3b31
随意 <cosmictinydust@hotmail.com>
2020-02-12 01:24:31 +0800

    实现了新增一个公司，里面包含了employee这个例子。 另外（重要）实现了一个集合的公司批量新增，然后返回一个可以查询新增后这个集合中的所有公司的链接至响应头。

---
+++ Routine.Api/Controllers/CompanycollectionsController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Entities;
+using Routine.Api.Helpers;
+using Routine.Api.Models;
+using Routine.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/companycollections")]
+    public class CompanycollectionsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompanycollectionsController(IMapper mapper, ICompanyRepository companyRepository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
+        }
+
+        [HttpGet("({ids})", Name = nameof(GetcompanyCollection))]
+        public async Task<IActionResult> GetcompanyCollection(
+            [FromRoute]
+            [ModelBinder(BinderType =typeof(ArrayModelBinder))]     //使用继承于IModelBinder接口的一个Helper实现参数绑定类似 "1,2,3" 的字符串
+            IEnumerable<Guid> ids
+            )
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var entities = await _companyRepository.GetCompaniesAsync(ids);
+            if (ids.Count() != entities.Count())
+            {
+                return NotFound();
+            }
+            var dtosToReturn = _mapper.Map<IEnumerable<CompanyDto>>(entities);
+            return Ok(dtosToReturn);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> CreateCompanyCollection(
+            IEnumerable<CompanyAddDto> companyCollection)
+        {
+            var companyEntities = _mapper.Map<IEnumerable<Company>>(companyCollection);
+            foreach (var company in companyEntities)
+            {
+                _companyRepository.AddCompany(company);
+            }
+            await _companyRepository.SaveAsync();
+
+            var dtoToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+
+            var idsString = string.Join(",", dtoToReturn.Select(x => x.ID));  //生成中间用","分隔的Guid字符串
+
+            return CreatedAtRoute(nameof(GetcompanyCollection), new { ids = idsString }, dtoToReturn);
+
+        }
+
+    }
+}
--- Routine.Api/Models/CompanyAddDto.cs
+++ Routine.Api/Models/CompanyAddDto.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace Routine.Api.Models
 {
     public class CompanyAddDto
     {
         public string Name { get; set; }
         public string Introduction { get; set; }
+
+        public ICollection<EmployeeAddDto> Employees { get; set; } = new List<EmployeeAddDto>();  //这里的 new List...的写法是防止使用时出现空引用类型的 据说是C#6.0后的新功能
+        //员工集合的名称最好与Company中对应的员工集合的名字一致（这例子中就是Employees）因为CompanyAddDto与Company及EmployeeAddDto与Employee都是已经映射好了的，如果这里的员工集合的名称也致的话，就不需要的Automap的Profile中映射时写一一对应的关系，直接默认即可。
     }
 }
